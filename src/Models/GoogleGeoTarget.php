e488cc3e2030fd619014d5c3702b33793371a212
Stef van Esch <stef@marshmallow.dev>
2020-09-09 14:46:33 +0200

    Use marshmallow/sluggable

--- src/Models/GoogleGeoTarget.php
+++ src/Models/GoogleGeoTarget.php
@@ -2,8 +2,8 @@
 
 namespace Marshmallow\Datasets\GoogleGeoTargets\Models;
 
-use Spatie\Sluggable\HasSlug;
-use Spatie\Sluggable\SlugOptions;
+use Marshmallow\Sluggable\HasSlug;
+use Marshmallow\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
 use Marshmallow\Datasets\GoogleGeoTargets\Models\GoogleGeoTargetType;
 
